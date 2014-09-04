a333abc8f31acc8ff31761fcea02ecec42e2650e
Antikainen Olli P <olantika@da1-cs-b221-12.helsinki.fi>
2014-09-04 14:32:18 +0300

    eka muokkaus

---
+++ supergapper/src/main/java/ohmi23/supergapper/App.java
@@ -0,0 +1,13 @@
+package ohmi23.supergapper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
