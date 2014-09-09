93fbb7aeb844a9ce4e0d06c7e2942550f4415522
Olli Antikainen <olli.antikainen@helsinki.fi>
2014-09-09 13:47:54 +0300

    Fuksilappari testi.

--- supergapper/src/main/java/ohmi23/supergapper/App.java
+++ supergapper/src/main/java/ohmi23/supergapper/App.java
@@ -1,15 +1,9 @@
 package ohmi23.supergapper;
 
-// testikommentti
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Morjensta. Tervetuloa pelaamaan Super-Gapperia. );
     }
 }
