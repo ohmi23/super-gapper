78a1bcbebceec2667caa6cad3b5757ce7db0ea94
Olli Antikainen <olli.antikainen@helsinki.fi>
2014-09-11 13:43:33 +0300

    a

--- supergapper/src/main/java/ohmi23/supergapper/Gapperpeli.java
+++ supergapper/src/main/java/ohmi23/supergapper/Gapperpeli.java
@@ -1,15 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package ohmi23.supergapper;
 
-/**
- *
- * @author olantika
- */
 public class Gapperpeli {
+
+    private int korkeus;
+    private int leveys;
+    private boolean jatkuu;
+    //private Gapper gapper;
+    //private Seeker seeker;
+    //private Kentta kentta;
+    
+    
+    public Gapperpeli(int leveys, int korkeus) {
+        
+        this.leveys = leveys;
+        this.korkeus = korkeus;
+        this.jatkuu = true;
+        
+        
+    }
+    
+    
+    // To do: 
+    
+    // JUnit testit testaavat onko kenttä oikean kokoinen.
+    
+    // Matopeli luo kentän käyttäen Tronviiva-olioita.
+    // Tronviiva-olion sisälle tulee lista kohdista joissa on käyty.
+    
+    // Näppäimistönkuuntelija ja käyttöliittymä-luokat.
+    
+    // Matopeli luo Gapper-olion. (extends Ukko)
+    // Matopeli luo Seeker-olion. (extends Ukko)
+    // Matopeli luo SuperSeeker olion jolla parempi tekoäly (Extends Seeker)
+    // Matopeli alkaa laskemaan kentän numeroa, pisteitä ja elämiä.
+    // Matopeli tarkkailee Gapper-Seeker törmäyksiä.
+    
+    // 
+    
+    // Matopeli käynnistää pelin
+    // Matopeli havaitsee törmäyksen ja vähentää elämän, kun elämät loppuvat,
+    // Matopeli kirjaa pisteet.
+    // Matopeli päättyy.
     
 }
