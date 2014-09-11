c96963a879404e25eecf4410ea05f8a32bcb34b8
Olli Antikainen <olli.antikainen@helsinki.fi>
2014-09-11 13:14:58 +0300

    Lisätty muutamia luokkia

---
+++ supergapper/src/main/java/ohmi23/supergapper/Suunta.java
@@ -0,0 +1,7 @@
+package ohmi23.supergapper;
+    
+public enum Suunta {
+ 
+    YLOS, OIKEA, ALAS, VASEN, TYHJA;
+    
+}
---
+++ supergapper/src/main/java/ohmi23/supergapper/Tronviiva.java
@@ -0,0 +1,15 @@
+package ohmi23.supergapper;
+
+public class Tronviiva {
+    
+    public int koko;
+    public boolean varitys;
+    
+
+    public Tronviiva(int koko) {
+        this.koko = koko;
+        this.varitys = false;
+    }
+    
+    
+}
