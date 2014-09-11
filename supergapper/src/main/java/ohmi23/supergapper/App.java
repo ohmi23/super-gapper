73cb8ac61540fe04a5b0139c155744ccfeba594e
Olli Antikainen <olli.antikainen@helsinki.fi>
2014-09-11 13:44:08 +0300

    a

--- supergapper/src/main/java/ohmi23/supergapper/App.java
+++ supergapper/src/main/java/ohmi23/supergapper/App.java
@@ -4,6 +4,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Morjensta. Tervetuloa pelaamaan Super-Gapperia. );
+        System.out.println( "Morjensta. Tervetuloa pelaamaan Super-Gapperia. );"
+        Gapperpeli gapperpeli = new Gapperpeli(3,3); 
+        // Käynnistetään gapperpeli 3 x 3 kokoisena.
+                
+                
     }
 }
