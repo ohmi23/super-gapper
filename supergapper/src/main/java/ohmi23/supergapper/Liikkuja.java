2a2549a83405800f2fe1706774e4546e62b8d0b1
Olli Antikainen <olli.antikainen@helsinki.fi>
2014-09-11 13:43:49 +0300

    a

---
+++ supergapper/src/main/java/ohmi23/supergapper/Liikkuja.java
@@ -0,0 +1,21 @@
+package ohmi23.supergapper;
+
+public class Liikkuja {
+
+    int x;
+    int y;
+
+    public Liikkuja(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+}
