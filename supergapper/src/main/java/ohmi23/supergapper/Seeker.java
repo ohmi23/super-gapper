7de390bb6faf5ecfcdc40493c3dd0c983cd3c607
Olli Antikainen <olli.antikainen@helsinki.fi>
2014-09-16 12:02:20 +0300

    a

--- supergapper/src/main/java/ohmi23/supergapper/Seeker.java
+++ supergapper/src/main/java/ohmi23/supergapper/Seeker.java
@@ -2,8 +2,8 @@ package ohmi23.supergapper;
 
 public class Seeker extends Liikkuja {
     
-    public Seeker(int x, int y) {
-        super(x, y);
+    public Seeker(int x, int y, Suunta suunta) {
+        super(x, y, suunta);
     }
     
 }
