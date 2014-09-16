1add013f9d4e5d4e7906a2d5d6911895dd632d66
Olli Antikainen <olli.antikainen@helsinki.fi>
2014-09-16 12:01:46 +0300

    a

--- supergapper/src/main/java/ohmi23/supergapper/Gapper.java
+++ supergapper/src/main/java/ohmi23/supergapper/Gapper.java
@@ -2,8 +2,8 @@ package ohmi23.supergapper;
 
 public class Gapper extends Liikkuja {
     
-    public Gapper(int x, int y) {
-        super(x, y);
+    public Gapper(int x, int y, Suunta suunta) {
+        super(x, y, suunta);
     }
     
 }
