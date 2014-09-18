6ce96af99c04054948ebd700942ce5fd427ef41d
Olli Antikainen <olli.antikainen@helsinki.fi>
2014-09-18 15:49:17 +0300

    Aletaan implementoimaan kentän ja tronviivan väliin ruutua

---
+++ supergapper/src/main/java/ohmi23/supergapper/Pistelaskuri.java
@@ -0,0 +1,11 @@
+
+package ohmi23.supergapper;
+
+public class Pistelaskuri {
+    
+    // tuntee kentän ja kentän ruudut.
+    // päivittää pistelukua
+    // ilmoittaa kentän valmistumisesta
+    // logiikka: kun ruudun sivut on käyty, sytytetään ruudun pistevalo.
+    // 
+}
