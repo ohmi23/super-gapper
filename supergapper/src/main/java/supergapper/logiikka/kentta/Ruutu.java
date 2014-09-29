d4b33aa10658247c81b0e257daf03d3ed4918d4e
Olli Antikainen <olli.antikainen@helsinki.fi>
2014-09-29 16:01:14 +0300

    lisätty ruutuun pistestatus

--- supergapper/src/main/java/supergapper/logiikka/kentta/Ruutu.java
+++ supergapper/src/main/java/supergapper/logiikka/kentta/Ruutu.java
@@ -1,16 +1,26 @@
 package supergapper.logiikka.kentta;
 
 public class Ruutu {
-    
-    
+
+    private int pisteluku;
+
     public Ruutu() {
-        
+        this.pisteluku = 0;
     }
-    
-    
-    
-   
-    
-    
-    
+
+    public void nollaaPisteet() {
+        this.pisteluku = 0;
+    }
+
+    public void setPisteluku50() {
+        this.pisteluku = 50;
+    }
+
+    public void setPisteluku100() {
+        this.pisteluku = 100;
+    }
+
+    public int getPisteluku() {
+        return pisteluku;
+    }
 }
